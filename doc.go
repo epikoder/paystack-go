fbf96d80dc810eaa5305afe48254377cef4e67bf
epikoder <efedua.bell@gmail.com>
2022-09-07 14:33:32 +0100

    fix: unmarshal error metadata map

--- doc.go
+++ doc.go
@@ -6,7 +6,7 @@ documentation at https://developers.paystack.co/v1.0/reference.
 
 Usage:
 
-	import "github.com/rpip/paystack-go"
+	import "github.com/epikoder/paystack-go"
 
 	apiKey := "sk_test_b748a89ad84f35c2f1a8b81681f956274de048bb"
 
